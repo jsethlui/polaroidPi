dad0afe0f27d0500976c76df08aeb55073723159
Jeremy Louie <jeremysf74@yahoo.com>
2022-08-15 11:40:35 -0700

    Added playlist menu and Spotify link opening

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import sys
 import json
 import spotipy
 import webbrowser
+import pprint as pp
 
 def main():
     with open("./config.json") as j:
@@ -18,13 +19,16 @@ def main():
 
     user = spotifyObject.current_user()
     playlists = spotifyObject.current_user_playlists()["items"]
-    
-    print("Enter playlist: ")
+
     for i in range(len(playlists)):
         s  = str(i)
         s += " " + str(playlists[i]["name"]) 
         print(s)
+    opt = int(input("Enter playlist: "))
+    link = playlists[opt]["external_urls"]["spotify"]
+    webbrowser.open(link)
 
+    print("Exiting...")
     sys.exit(0)
 
 if __name__ == "__main__":
